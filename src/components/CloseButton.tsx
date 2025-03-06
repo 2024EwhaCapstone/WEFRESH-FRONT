c15e5720baded5b6c35e646c26569f7e58622bcc
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2025-03-06 16:05:01 +0900

    feat: CloseButton 수정

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -3,28 +3,28 @@ import { TouchableOpacity, Image, StyleSheet, StyleProp, ViewStyle } from 'react
 import { useNavigation } from '@react-navigation/native';
 
 interface CloseButtonProps {
-  onPress?: () => void;  // 버튼 클릭 이벤트
-  style?: StyleProp<ViewStyle>; // 외부 스타일 적용 가능
-  backgroundColor?: string; // 배경 색상 지정 가능
-  iconColor?: string; // 아이콘 색상 지정 가능
+  onPress?: () => void;  
+  style?: StyleProp<ViewStyle>; 
+  backgroundColor?: string; 
+  iconColor?: string; 
 }
 
 const CloseButton: React.FC<CloseButtonProps> = ({ 
   onPress, 
   style, 
-  backgroundColor = '#4CAF50',  // 기본값: 초록색
-  iconColor = 'white'  // 기본값: 흰색
+  backgroundColor = '#4CAF50',  
+  iconColor = 'white' 
 }) => {
   const navigation = useNavigation();
 
   return (
     <TouchableOpacity 
-      style={[styles.closeButton, { backgroundColor }, style]} // 배경색 적용
+      style={[styles.closeButton, { backgroundColor }, style]} 
       onPress={onPress ? onPress : () => navigation.goBack()}
     >
       <Image
         source={require('../assets/icons/closeicon.png')}
-        style={[styles.image, { tintColor: iconColor }]} // 아이콘 색 적용
+        style={[styles.image, { tintColor: iconColor }]} 
       />
     </TouchableOpacity>
   );
