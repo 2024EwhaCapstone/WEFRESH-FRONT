c0a79789c7d4f80b1220faf74d503c947b88a4f8
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2025-04-10 21:44:17 +0900

    fix: 초기 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parserOptions: {
-    requireConfigFile: false // Babel 설정 파일 강제 요구 비활성화
+    requireConfigFile: false 
   }
 };
