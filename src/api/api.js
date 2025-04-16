1f8f16a1e944607a9eca4e4fe1d36cb2fdf07c91
chaewonkim <kimlove9182@gmail.com>
2025-04-17 00:48:32 +0900

    fix: 재료 태그 수정

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'https://wefresh.store';
-const ACCESS_TOKEN = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpYXQiOjE3NDM3NzA1ODYsImV4cCI6MTc0NDk4MDE4NiwidXNlcklkIjoxfQ.pnvGUa9G6hLHnvw9lSQFWy13XsmS50WWN74KjtJ8HmKACmWOw1k_sQpu5LTvA6xH`;
+const ACCESS_TOKEN = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpYXQiOjE3NDQ4MTI1NjksImV4cCI6MTc0NjAyMjE2OSwidXNlcklkIjoxfQ.QWnp2FFu1DsDDft7h9alCd-eTkPB0o3Trv8wl6koRkFaUQ0_nLP88SjHBoyZm3Pd`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
