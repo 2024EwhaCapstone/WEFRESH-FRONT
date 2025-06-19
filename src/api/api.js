5730230e143feddf7d92007a3353c472e89d2770
Seungyeon Kim <sungyeon1227@gmail.com>
2025-06-19 22:07:53 +0900

    fix: access token

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'https://wefresh.store';
-const ACCESS_TOKEN = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpYXQiOjE3NTAzMzMzNDYsImV4cCI6MTc1MjkyNTM0NiwidXNlcklkIjoxfQ.TTLMHMkhMUni6o4OZefuwsCr5RdS4GoTP7a8rB1fzBTIYYbc-GfCt6K7uoqbrUUW`;
+const ACCESS_TOKEN = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzM4NCJ9.eyJpYXQiOjE3NTAzMzc4ODMsImV4cCI6MTc1MjkyOTg4MywidXNlcklkIjoxfQ.-nYfkFiSkA5UgRi924YF4msAuW-roSQctbMUlezqF7pFhYfOxz34enYg90ZHSYoO`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
